6170615cebcc2efe51a887734a226b44d8c8a54b
joeeames <joeeames@gmail.com>
2013-11-08 18:38:22 -0700

    more ui changes

--- public/app/app.js
+++ public/app/app.js
@@ -1,7 +1,7 @@
 angular.module('app', ['ngResource', 'ngRoute']);
 
 angular.module('app').config(function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
+//  $locationProvider.html5Mode(true);
   $routeProvider
     .when('/', { templateUrl: '/partials/home', controller: 'homeCtrl' })
     //.otherwise({redirectTo: '/'});
