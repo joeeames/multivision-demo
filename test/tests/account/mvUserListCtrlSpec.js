ae3b2c17dc4271ff526ab505361e726a683b2b8b
joeeames <joeeames@gmail.com>
2013-11-13 13:48:43 -0700

    finished testing

---
+++ test/tests/account/mvUserListCtrlSpec.js
@@ -0,0 +1,21 @@
+describe('mvUserListCtrl', function() {
+
+  beforeEach(module('app'));
+
+  beforeEach(inject(function($controller) {
+    this.$controller = $controller;
+  }));
+
+  describe('constructor', function() {
+    it('should set the users collection to the result of the User service query', function() {
+      var mockUserSvc = sinon.stub({query: function() {}});
+      var mockUsersCollection = [];
+      mockUserSvc.query.returns(mockUsersCollection);
+      var mockScope = {}
+
+      this.$controller('mvUserListCtrl', {$scope: mockScope, mvUser: mockUserSvc});
+
+      expect(mockScope.users).to.equal(mockUsersCollection);
+    });
+  })
+})
