bd5fe4ea0afe9133961c95e8f60ce2854d346755
Jason S. Jones <jsjones96@gmail.com>
2015-01-29 21:12:35 -0800

    change route param in url to match paramDefaults

--- public/app/courses/mvCourse.js
+++ public/app/courses/mvCourse.js
@@ -1,5 +1,5 @@
 angular.module('app').factory('mvCourse', function($resource) {
-  var CourseResource = $resource('/api/courses/:id', {_id: "@id"}, {
+  var CourseResource = $resource('/api/courses/:_id', {_id: "@id"}, {
     update: {method:'PUT',isArray:false}
   });
 
